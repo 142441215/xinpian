c5a15dfa0211a3aea23b3f90004af9f10f4f5d10
严浩 <1424412158@qq.com>
2020-04-17 15:40:18 +0800

    2

--- xinpian/xinpian.cpp
+++ xinpian/xinpian.cpp
@@ -1,20 +1,37 @@
-﻿// xinpian.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-#include <iostream>
-
+﻿#include<iostream>
+#include<opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
 int main()
 {
-    std::cout << "Hello World!\n";
+	Mat src = imread("c://project//die_on_chip.png", 0);
+	Mat dist;
+	Mat proc;
+	Mat proc1;
+	Mat labelmat;
+	Mat statsmat;
+	Mat centermat;
+	Mat dstImage;
+	threshold(src, dist, 150, 255, THRESH_BINARY);
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierarchy;
+	findContours(dist, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	for (int i = 0; i < contours.size(); i++)
+	{
+		RotatedRect rbox = minAreaRect(contours[i]);
+		float width = rbox.size.width;
+		float height = rbox.size.height;
+		float test = width / height;
+		cout << test << endl;
+		if (test > 0.9&&test < 1.1) {
+			drawContours(src, contours, i, Scalar(255, 255, 0), 1, 8);
+			Point2f vtx[4];
+			rbox.points(vtx);
+			for (int j = 0; j < 4; ++j) {
+				line(src, vtx[j], vtx[j < 3 ? j + 1 : 0], Scalar(0, 0, 255), 2, CV_AA);
+			}
+		}
+	}
+	imshow("src", src);
+	waitKey(0);
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
